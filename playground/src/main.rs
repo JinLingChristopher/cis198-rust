7c54d55f55bd4c967627cf94a51710e21d4ac24c
3AceShowHand <jinl1037@hotmail.com>
2021-07-09 00:07:05 +0800

    rename to rust-gym

---
+++ playground/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    // let mut a = 10;
+    // println! ("the number is {}", a);
+
+    // a = 15;
+    // println! ("the number is {}", a);
+
+    let number = 5;
+    let number = number + 5;
+    let number = number * 2;
+
+    println! ("the number is {}", number);
+
+}
