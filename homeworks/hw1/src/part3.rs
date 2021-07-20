666610d7f0de2362a2c9e6ab5268a12c99841fe0
3AceShowHand <jinl1037@hotmail.com>
2021-07-20 13:54:35 +0800

    finish part3, ownership is fuck hard.

--- homeworks/hw1/src/part3.rs
+++ homeworks/hw1/src/part3.rs
@@ -22,6 +22,7 @@
     Q: A common source of error in swap implementations is failing to work if
        the two references are the same. Why don't you need to worry about this
        case in Rust?
+    A: cannot borrow a variable as mutable more than once at a time.
 
     (Try writing a unit test where they are both
     the same, i.e. swap_ints(&mut x, &mut x).)
@@ -44,16 +45,18 @@ fn swap_ints_test() {
 /*
     Problem 2: String duplication
 */
-// #[test]
-// fn copy_string_test() {
-//     let str1 = String::from("foo");
-//     let str2 = str1;
-//     assert_eq!(str1, str2);
-// }
+#[test]
+fn copy_string_test() {
+    let str1 = String::from("foo");
+    let str2 = str1.clone();
+    assert_eq!(str1, str2);
+}
 // This test doesn't work. Fix it by copying strings properly.
 // Q1. What went wrong?
+// A: value borrowed here after move.
 
 // Q2. How come it works fine here?
+// A: i32 is allocated on stack
 #[test]
 fn copy_int_test() {
     let i1 = 1;
@@ -115,18 +118,48 @@ fn copy_me_test2() {
     such as "unsafe code" can be used to turn off Rust's safety and lifetime
     checks.)
 */
-// fn new_ref_string() -> &String {
-//     unimplemented!();
+// fn new_ref_string<'a>() -> &'a String {
+//     let a = String::from("chio");
+//     return &a;
 // }
 //
-// fn new_ref_str() -> &str {
-//     unimplemented!();
+// #[test]
+// fn test_new_ref_string() {
+//     let obtained = new_ref_string().as_str();
+//     assert_eq!(obtained, "chio".to_string().as_str())
 // }
-//
-// // The same function from part2
-// fn pick_longest2(s1: &str, s2: &str) -> &str {
-//     unimplemented!()
-// }
+// A: not possible. a reference to a local variable cannot be returned.
+
+fn  new_ref_str<'a>() -> &'a str {
+    "chio"
+}
+
+#[test]
+fn test_new_ref_str() {
+    let a = new_ref_str();
+    assert_eq!(a, "chio");
+}
+
+// The same function from part2
+fn pick_longest2<'a>(s1: &'a str, s2: &'a str) -> &'a str {
+    if s1.len() > s2.len() {
+        return s1;
+    }
+    s2
+}
+
+#[test]
+fn test_pick_longest2() {
+    let s1 = "hello";
+    let s2 = "world";
+    assert_eq!(pick_longest2(s1, s2), "world");
+
+    let s3 = "king";
+    assert_eq!(pick_longest2(s1, s3), s1);
+}
+
+// A: we should not able to return reference from a function,
+// since it's out of stack variable's lifetime.
 
 /*
     Problem 5: Using functions with lifetimes
@@ -142,12 +175,44 @@ fn copy_me_test2() {
     Q2. What are the pros and cons of v1 and v2?
 */
 
+// result move each item into it in the loop, no real memory copy.
+// rust make shallow copy and let previous invalidate, it's move.
 fn pick_longest_in_v1(v: Vec<String>) -> String {
-    unimplemented!()
+    let mut result = "".to_string();
+    for item in v {
+        if item.len() > result.len() {
+            result = item
+        }
+    }
+    return result
 }
 
+#[test]
+fn test_pick_longest_in_v1() {
+    let words = vec!["yu".to_string(), "xuan".to_string(), "chio".to_string(),
+                     "pretty".to_string(), "gorgeous".to_string()];
+    let result = pick_longest_in_v1(words);
+    assert_eq!("gorgeous".to_string(), result);
+}
+
+// &str located in static memory area, result is a local variable on the stack
+// it will copy item each loop, and then move to the caller function
 fn pick_longest_in_v2(v: Vec<&str>) -> &str {
-    unimplemented!()
+    let mut result = "";
+    for item in v {
+        if item.len() > result.len() {
+            result = item
+        }
+    }
+
+    result
+}
+
+#[test]
+fn test_pick_longest_in_v2() {
+    let words = vec!["This", "is", "a", "rust", "programming", "course"];
+    let result = pick_longest_in_v2(words);
+    assert_eq!("programming", result)
 }
 
 /*
@@ -162,17 +227,19 @@ fn pick_longest_in_v2(v: Vec<&str>) -> &str {
 */
 
 fn pad_with_zeros_v1(v: Vec<usize>, desired_len: usize) -> Vec<usize> {
-    unimplemented!()
-    // debug_assert_eq!(result.len(), desired_len);
+    let zeros = vec![0usize].repeat(desired_len - v.len());
+    let result = [v, zeros].concat();
+    debug_assert_eq!(result.len(), desired_len);
+    result
 }
 
 fn pad_with_zeros_v2(slice: &[usize], desired_len: usize) -> Vec<usize> {
-    unimplemented!()
+    pad_with_zeros_v1(Vec::from(slice), desired_len)
     // debug_assert_eq!(result.len(), desired_len);
 }
 
 fn pad_with_zeros_v3(v: &mut Vec<usize>, desired_len: usize) {
-    unimplemented!()
+    v.resize(desired_len, 0)
     // debug_assert_eq!(v.len(), desired_len);
 }
 
@@ -218,13 +285,29 @@ fn test_pad_twice_v3() {
 */
 
 fn append_row(grid: &mut Vec<Vec<bool>>, row: Vec<bool>) {
-    unimplemented!()
+    grid.push(row)
+}
+
+#[test]
+fn test_append_row() {
+    let grid = &mut vec![vec![true], vec![false]];
+    append_row(grid, vec![true]);
+    assert_eq!(grid, &mut vec![vec![true], vec![false], vec![true]]);
 }
 
 fn is_first_row(grid: &[Vec<bool>], row: &[bool]) -> bool {
     // Check if row is the first row in grid
     // Remember to handle the case when grid is empty
-    return true
+    match grid.first() {
+        Some(r) => *r == row.to_vec(),
+        None => false,
+    }
+}
+
+#[test]
+fn test_is_first_row() {
+    let grid = vec![vec![true], vec![false]];
+    assert_eq!(is_first_row(&grid, &vec![false]), false);
 }
 
 /*
@@ -243,18 +326,40 @@ use std::collections::HashMap;
 // https://doc.rust-lang.org/std/collections/struct.HashMap.html
 
 fn vector_to_hashmap(v: &[(i32, String)]) -> HashMap<i32, String> {
-    unimplemented!();
+    let result = v.into_iter().cloned().collect();
+    result
+}
+
+#[test]
+fn test_vector_to_hashmap() {
+    let pairs = vec![(1, "haha".to_string()), (2, "maybe".to_string())];
+    let obtained = vector_to_hashmap(&pairs);
+
+    let mut expected = HashMap::new();
+    expected.insert(1, "haha".to_string());
+    expected.insert(2, "maybe".to_string());
+
+    assert_eq!(obtained, expected);
 }
 
 // Now rewrite this function to delete all entries in hashmap where the keys
 // are negative.
 fn delete_negative_keys(h: &mut HashMap<i32, i32>) {
-    // This fails, uncomment to see error.
-    // for k in h.keys() {
-    //     if *k < 0 {
-    //         h.remove(k);
-    //     }
-    // }
+    h.retain(|k, _| *k >= 0)
+}
+
+#[test]
+fn test_delete_negative_keys() {
+    let mut map = HashMap::new();
+    map.insert(1, 2);
+    map.insert(2, 3);
+
+    let expected = map.clone();
+
+    map.insert(-1, 0);
+
+    delete_negative_keys(&mut map);
+    assert_eq!(map, expected);
 }
 
 /*
@@ -280,5 +385,30 @@ fn merge_maps(
     merged: &mut HashMap<String, String>,
     add: HashMap<String,String>
 ) {
-    unimplemented!()
+    for (k, v) in add.into_iter() {
+        merged.entry(k).and_modify(|val| val.push_str(v.as_str())).or_insert(v);
+    }
+}
+
+#[test]
+fn test_merge_maps() {
+    let mut merged: &mut HashMap<String, String> = &mut HashMap::new();
+    merged.insert("a".to_string(), "1".to_string());
+
+    let mut add: HashMap<String, String> = HashMap::new();
+    add.insert("b".to_string(), "2".to_string());
+    add.insert("a".to_string(), "3".to_string());
+
+    merge_maps(&mut merged, add);
+
+    let mut expected: HashMap<String, String> = HashMap::new();
+    expected.insert("a".to_string(), "13".to_string());
+    expected.insert("b".to_string(), "2".to_string());
+
+    for (key, val) in expected.into_iter() {
+        match merged.get(&key) {
+            Some(merged_value) => assert_eq!(val, *merged_value),
+            None => assert!(false),
+        }
+    }
 }
